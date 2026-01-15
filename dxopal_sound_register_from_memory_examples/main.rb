d6585c6450ef02045b4c2efac62df3ee02bbc98f
sonota88 <yosiot8753@gmail.com>
2026-01-15 23:04:01 +0900

    Add example 6, 7 (dxopal_sound_register_from_memory_examples/main.rb)

--- dxopal_sound_register_from_memory_examples/main.rb
+++ dxopal_sound_register_from_memory_examples/main.rb
@@ -70,6 +70,14 @@ BUTTONS = [
     onclick: ->(){ play Sound[:ms5] }
   },
   {
+    name: "ms6", text: "example 6",
+    onclick: ->(){ play Sound[:ms6] }
+  },
+  {
+    name: "ms7", text: "example 7",
+    onclick: ->(){ play Sound[:ms7] }
+  },
+  {
     name: "nes_tri_220", text: "NES triangle 220Hz",
     onclick: ->(){ play Sound[:nes_tri_220] }
   },
@@ -144,6 +152,17 @@ def osc_pulse(x, duty_ratio = 0.5)
   else
     -1.0
   end
+end
+
+def wavetable_normalize(wt)
+  max = wt.map(&:abs).max
+  wt.map { |x| x.to_f / max }
+end
+
+def wavetable_get(wt, x)
+  ratio = (x.to_f / TWO_PI) % 1
+  i = (wt.size * ratio).floor
+  wt[i]
 end
 
 # 4c: 60 / 4a: 69
@@ -271,6 +290,76 @@ def make_sound_5
     x_delta = TWO_PI * freq * MemorySound::SEC_PER_SAMPLE
     x += x_delta
     osc_pulse(x, 0.125) * vol * MASTER_VOLUME
+  }
+
+  memory_sound
+end
+
+def make_sound_6
+  samples = []
+  dur_msec = 300
+  dur_sec = dur_msec.to_f / 1000
+  srate = MemorySound::SAMPLING_RATE
+  num_samples = (srate * dur_sec).floor
+  sec_per_sample = 1.0 / srate
+  vol = 0.7
+
+  freqs = [0, 4, 7, 11]
+            .map { |nn| nn + 72 }
+            .map { |nn| to_hz(nn) }
+  freqs = freqs * 3
+
+  x = 0.0
+  memory_sound = MemorySound.generate(dur_msec) { |i, t|
+    ratio = i.to_f / num_samples
+    ratio_inv = 1.0 - ratio
+
+    v = osc_pulse(x, 0.25)
+
+    fi = (freqs.size * ratio).floor
+    freq = freqs[fi]
+
+    x_delta = TWO_PI * freq * sec_per_sample
+    x += x_delta
+
+    v * ratio_inv * vol * MASTER_VOLUME
+  }
+
+  memory_sound
+end
+
+def make_sound_7
+  samples = []
+  dur_msec = 500
+  dur_sec = dur_msec.to_f / 1000
+  srate = MemorySound::SAMPLING_RATE
+  num_samples = (srate * dur_sec).floor
+  sec_per_sample = 1.0 / srate
+  vol = 0.8
+
+  wt = wavetable_normalize(
+    [7, -7, 7, -7, -0, -0, -0, -0,]
+  )
+
+  freqs = [0, 1, 2, 3]
+            .map { |nn| nn * 0.5 }
+            .map { |nn| nn + 60 }
+            .map { |nn| to_hz(nn) }
+  freqs = freqs * 3
+
+  x = 0.0
+  memory_sound = MemorySound.generate(dur_msec) { |i, t|
+    ratio = i.to_f / num_samples
+    ratio_inv = 1.0 - ratio
+
+    v = wavetable_get(wt, x)
+
+    fi = (freqs.size * ratio).floor
+    freq = freqs[fi]
+    x_delta = TWO_PI * freq * sec_per_sample
+    x += x_delta
+
+    v * ratio_inv * vol * MASTER_VOLUME
   }
 
   memory_sound
@@ -463,6 +552,8 @@ def init_010
   sound_register_from_memory(:ms3, make_sound_3)
   sound_register_from_memory(:ms4, make_sound_4)
   sound_register_from_memory(:ms5, make_sound_5)
+  sound_register_from_memory(:ms6, make_sound_6)
+  sound_register_from_memory(:ms7, make_sound_7)
 
   sound_register_from_memory(:nes_tri_220, make_sound_nes_tri(220))
   sound_register_from_memory(:nes_tri_110, make_sound_nes_tri(110))
