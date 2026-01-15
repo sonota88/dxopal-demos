64274917455f10dae0df135ed8055d29f15e4f8d
sonota88 <yosiot8753@gmail.com>
2026-01-15 22:32:56 +0900

    __wip 2026-01-15 22:32:56 (main.rb)

--- dxopal_sound_register_from_memory_examples/main.rb
+++ dxopal_sound_register_from_memory_examples/main.rb
@@ -627,7 +627,7 @@ def main
         Window.draw_font(10, y + 6, button[:text], FONT_MAIN, color: C_WHITE)
       end
 
-      Window.draw_font(WIN_W - 40, 10, "#{Input.mouse_y.inspect}", FONT_MAIN, color: [80,80,80])
+      Window.draw_font(WIN_W - 40, 10, "y=#{Input.mouse_y.inspect}", FONT_MAIN, color: [80,80,80])
     end
   end
 end
